72a29869b12ae24d074f4e57cdc3e00c7796ec27
M-boop-dt <Mohammedpopo87@gmail.com>
2025-04-19 23:53:52 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -28,7 +28,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "private",
   OWNER_NAME: process.env.OWNER_NAME || "Marisel",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "218942841878",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "255760317060",
   SUDO_NUMBER: process.env.SUDO_NUMBER || "254740007567",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false,
